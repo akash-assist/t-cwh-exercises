0e101bc36b8e7d47ea86d28f16241073e1acd027
akash-assist <akash.assist@gmail.com>
2024-01-24 08:34:47 +0530

    add op cond js

---
+++ tutorials/vid56condjs/index.js
@@ -0,0 +1,73 @@
+console.log("vid 56");
+
+let age = 5;
+
+if(age>9){
+    console.log("you are good for ps5");
+}
+else{
+    console.log("sorry come back after " + (9-age) + " years");
+}
+
+let a = 5;
+let b = 4;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a**b);
+console.log(a%b);//modulus
+
+// assignment operators same as c++
+
+
+
+// comparison operators in javascript
+
+//checking..... equal to & not equal to...... is same as c++
+let c = "hello"
+console.log(a==b);
+console.log(a!=b);
+
+//it also checks type of operands along with value
+console.log(a===c);
+
+
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+console.log(true?a:b);
+
+
+let d = true;
+let e = false;
+
+console.log(!d);
+console.log(d&&e);
+console.log(d||e);
+
+
+//this is comment 
+
+
+
+/*
+this is also 
+a comment
+*/
+
+
+if(e){
+
+}
+else if(d){
+
+}
+else if(a){
+
+}
+else{
+
+}
+
