a44be6c55869263cd51b82fbbf424122398fa516
akash-assist <akash.assist@gmail.com>
2024-01-28 11:00:38 +0530

    add str tut js

---
+++ tutorials/vid60strjs/index.js
@@ -0,0 +1,72 @@
+
+
+console.log("Hello strings");
+
+let a = "akash";
+
+console.log(a);
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+
+
+
+console.log(a[5]);
+
+
+console.log(a.length);
+
+let friend = "anand";
+
+console.log("My name is " + a + " and My friend's name is " + friend);
+
+
+console.log(`My name is ${a} and my friend's name is ${friend}`);
+
+
+console.log(`My name is ${a} and my friend's name is ${friend}`);
+
+
+
+console.log(`aka"s"h`);
+
+//this is how to use escape sequence character
+console.log("aka\"s\"h"); //escape symbol
+console.log("aka'\nsh"); //new line
+console.log("aka'\tsh"); //tab
+console.log("aka\rsh"); //carriage return
+
+console.log("aka'sh");
+
+
+let b = "Mahendra Pratap Singh Pratap"
+
+console.log(b.length)
+console.log(b.toUpperCase());
+console.log(b.toLowerCase());
+
+console.log(b.slice(0,5));
+console.log(b.slice(5));
+
+console.log(b.replace("Pratap", "Kumar")); //only replace first occurence
+
+
+console.log(b.concat(a, "anand", "sarita"));
+
+
+//string is immutable {can't be changed}
+console.log(b);
+
+
+
+//functions 
+//indexOf
+//charAt
+//endsWith
+//starsWith
+
+
+
