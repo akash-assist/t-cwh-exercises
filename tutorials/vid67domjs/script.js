5696ea685c44d4a2bfb4f5cf41d14ce62ebacc8d
akash-assist <akash.assist@gmail.com>
2024-02-05 09:50:41 +0530

    add dom tut2 js

---
+++ tutorials/vid67domjs/script.js
@@ -0,0 +1,20 @@
+console.log("Hello World!");
+
+/*
+document.body
+
+
+document.body.childNodes
+
+
+document.body.childNodes[0]
+first child is #text node
+because there is space is part of document toolbar
+
+document.body.childNodes[1]
+this will be div.container
+
+document.body.childNodes[1].childNodes
+//to access childs of this node
+
+*/
