fe28069f980118a3ffe168abd0f30f234fd56f4f
akash-assist <akash.assist@gmail.com>
2024-02-06 08:26:28 +0530

    add js dom tut3

--- tutorials/vid67domjs/script.js
+++ tutorials/vid67domjs/script.js
@@ -9,12 +9,71 @@ document.body.childNodes
 
 document.body.childNodes[0]
 first child is #text node
-because there is space is part of document toolbar
+because there is space which is part of document
 
 document.body.childNodes[1]
 this will be div.container
 
 document.body.childNodes[1].childNodes
 //to access childs of this node
+
+
+
+let x = document.body.childNodes[1]; 
+
+x
+
+x.firstChild
+
+y.lastChild
+
+//now ignoring text box....that is accessing element child
+
+x.firstElementChild
+
+x.lastElementChild
+
+x.lastElementChild.style.color = "red"
+
+x.lastElementChild.style.backGroundColor = "red"
+
+
+
+//accessing parents
+
+x.parentElement
+
+document.body.firstElementChild
+
+//all nodes like comment node and text nodes
+document.body.firstElementChild.childNodes
+//to just access the elements only
+document.body.firstElementChild.children
+
+
+
+// comment in html is also treated as text nodes or comment node
+
+
+
+document.body.firstElementChild.children[0]
+document.body.firstElementChild.children[1]
+document.body.firstElementChild.children[2]
+document.body.firstElementChild.children[3]
+document.body.firstElementChild.children[4]
+
+
+
+document.body.firstElementChild.children[3].nextElementSibling
+document.body.firstElementChild.children[3].previousElementSibling
+document.body.firstElementChild.children[3].previousSibling
+
+
+document.body.firstElementChild.children[3].parentElement 
+
+
+//to access the table in html element
+document.body.children[1].rows
+
 
 */
